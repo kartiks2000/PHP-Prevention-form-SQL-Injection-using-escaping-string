b4870e09c5db106c62c2290194828a951de491eb
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-07 17:47:59 +0530

    Add files via upload

--- phpdemo/dem_process.php
+++ phpdemo/dem_process.php
@@ -9,8 +9,11 @@
         }
         else{
 
+            // Prevention against SQL Injection using escaping characters
             $name = mysqli_real_escape_string($connect,$name);
             $pswd = mysqli_real_escape_string($connect,$pswd);
+
+
 
             $query = "INSERT INTO users(username,password) VALUE('$name','$pswd')";
             $result = mysqli_query($connect,$query);
